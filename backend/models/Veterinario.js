b3547e2393448c81fd0d104dd52bb001b3c096c4
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-07-04 08:40:34 -0400

    Hasheamos los passwords con bcrypt

--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import generarID from "../helpers/generarID.js";
+
 const veterinarioSchema = mongoose.Schema({
   nombre: {
     type: String,
@@ -33,6 +35,16 @@ const veterinarioSchema = mongoose.Schema({
     type: Boolean,
     default: false,
   },
+});
+
+// Hashear los passwords
+veterinarioSchema.pre("save", async function (next) {
+  //si ya esta hasheado que se pase el salt
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
