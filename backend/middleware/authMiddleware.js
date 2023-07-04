4535f470065eabd00f9e1959107a5e482abb2013
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-07-04 11:00:53 -0400

    Creando rutas protegidas middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,5 @@
+const checkAuth = (req, res, next) => {
+  console.log("Desde mi middleware");
+  next();
+};
+export default checkAuth;
--- backend/routes/veterinarioRouter.js
+++ backend/routes/veterinarioRouter.js
@@ -5,15 +5,16 @@ import {
   confirmar,
   autenticar,
 } from "../controllers/veterinarioControllers.js";
+import checkAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", registrar);
 
-router.get("/perfil", perfil);
-
 router.get("/confirmar/:token", confirmar);
 
 router.post("/login", autenticar);
+
+router.get("/perfil", checkAuth, perfil);
 
 export default router;
