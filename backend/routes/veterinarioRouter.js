c766adb73cd02f4ef29475f0bb6c17e99c862909
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-07-04 15:49:39 -0400

    Creando el routing para resetear el password

--- backend/routes/veterinarioRouter.js
+++ backend/routes/veterinarioRouter.js
@@ -4,17 +4,29 @@ import {
   perfil,
   confirmar,
   autenticar,
+  olvidePassword,
+  comprobarToken,
+  nuevoPassword,
 } from "../controllers/veterinarioControllers.js";
 import checkAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-
+//Area publica
 router.post("/", registrar);
 
 router.get("/confirmar/:token", confirmar);
 
 router.post("/login", autenticar);
+//validar el email del usuario
+router.post("/olvide-password", olvidePassword);
+//para leer el token
+// router.get("/olvide-password/:token", comprobarToken);
+//almacenar el nueva password
+// router.post("/olvide-password/:token", nuevoPassword);
+//sintaxis simplificada;
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
 
+//Area privada
 router.get("/perfil", checkAuth, perfil);
 
 export default router;
