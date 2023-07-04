f5b1083064ae42a4f978b8218d3e403058cbddd4
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-07-04 12:06:23 -0400

    Comprobando el token en el middleware y creando enviroment en postman

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,17 @@
+import jwt from "jsonwebtoken";
 const checkAuth = (req, res, next) => {
-  console.log("Desde mi middleware");
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      console.log("Si tiene el token con Bearer");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   next();
 };
 export default checkAuth;
