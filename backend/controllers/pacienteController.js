27d6787be20cbbc513f42681b6be2e555f448da3
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-07-05 15:59:36 -0400

    Actualizar paciente

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -21,15 +21,43 @@ const obtenerUnPaciente = async (req, res) => {
   const { id } = req.params;
   const paciente = await Paciente.findById(id);
 
+  if (!paciente) {
+    return res.status(400).json({ meg: "Paciente no encontrado..." });
+  }
+
   if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
     return res.json({ meg: "Accion no valida..." });
   }
-  if (paciente) {
-    res.json(paciente);
-  }
+  //si existe el paciente
+  res.json(paciente);
 };
 
-const actualizarPaciente = async (req, res) => {};
+const actualizarPaciente = async (req, res) => {
+  const { id } = req.params;
+  const paciente = await Paciente.findById(id);
+
+  if (!paciente) {
+    return res.status(400).json({ meg: "Paciente no encontrado..." });
+  }
+
+  if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+    return res.json({ meg: "Accion no valida..." });
+  }
+
+  //Actualizar paciente
+  paciente.nombre = req.body.nombre ?? paciente.nombre;
+  paciente.propietario = req.body.propietario ?? paciente.propietario;
+  paciente.email = req.body.email ?? paciente.email;
+  paciente.fecha = req.body.fecha ?? paciente.fecha;
+  paciente.sintomas = req.body.sintomas ?? paciente.sintomas;
+
+  try {
+    const pacienteActualizado = await paciente.save();
+    res.json(pacienteActualizado);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const eliminarPaciente = async (req, res) => {};
 
