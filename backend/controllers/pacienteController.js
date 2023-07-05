059bfadcac21bb57d09f19ef0def7ec07e4ce25f
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-07-05 13:15:49 -0400

    Almacenando el Veterinario en el Registro del paciente

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -1,9 +1,11 @@
 import Paciente from "../models/Pacientes.js";
 
-const agregarPacientes = (req, res) => {
+const agregarPacientes = async (req, res) => {
   const paciente = new Paciente(req.body);
-
+  paciente.veterinario = req.veterinario._id;
   try {
+    const pacienteAlmacenado = await paciente.save();
+    res.json(pacienteAlmacenado);
   } catch (error) {
     console.log(error);
   }
--- backend/routes/pacienteRouter.js
+++ backend/routes/pacienteRouter.js
@@ -3,9 +3,10 @@ import {
   agregarPacientes,
   obtenerPacientes,
 } from "../controllers/pacienteController.js";
+import checkAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(agregarPacientes).get(obtenerPacientes);
+router.route("/").post(checkAuth, agregarPacientes).get(obtenerPacientes);
 
 export default router;
