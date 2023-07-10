a51ab78b27196febdee6f78240c6e1a48267570d
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-07-09 22:02:14 -0400

    Creando un Context de Autenticacion

---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect, createContext } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
+
+export { AuthProvider };
+export default AuthContext;
