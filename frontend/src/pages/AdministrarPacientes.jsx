dd76369d50eef808d239067bbd01f3a21023c5de
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-07-11 13:53:56 -0400

    Responsive Designe al Formulario y boton dinamico para mostrar

--- frontend/src/pages/AdministrarPacientes.jsx
+++ frontend/src/pages/AdministrarPacientes.jsx
@@ -1,10 +1,25 @@
 import Formulario from "../components/Formulario";
 import ListadoPacientes from "../components/ListadoPacientes";
 
+import { useState } from "react";
+
 const AdministrarPacientes = () => {
+  const [mostrarFormulario, setNostrarFormulario] = useState(false);
+
   return (
     <div className="flex flex-col lg:flex-row">
-      <div className="md:w-1/2 lg:w-2/5">
+      <button
+        type="button"
+        className="bg-indigo-600 text-white uppercase font-bold mx-10 p-3 rounded-md md:hidden"
+        onClick={() => setNostrarFormulario(!mostrarFormulario)}
+      >
+        {mostrarFormulario ? "Ocutar Formulario" : "Mostrar Formulario"}
+      </button>
+      <div
+        className={`${
+          mostrarFormulario ? "block" : "hidden"
+        } md:block md:w-1/2 lg:w-2/5`}
+      >
         <Formulario />
       </div>
       <div className="md:w-1/2 lg:h-3/5">
