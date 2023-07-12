707e0db4cae95c8f0d759ab43a55715f84652118
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-07-12 12:48:46 -0400

    Creando los componentes de EditarPerfil y CambiarPassword

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import Registrar from "./pages/Registrar";
 import OlvidePassword from "./pages/OlvidePassword";
 import NuevoPassword from "./pages/NuevoPassword";
 import ConfirmarCuenta from "./pages/ConfirmarCuenta";
+import EditarPerfil from "./components/EditarPerfil";
+import CambiarPassword from "./components/CambiarPassword";
 
 import { AuthProvider } from "./context/AuthProvider";
 import { PacientesProvider } from "./context/PacientesProvider";
@@ -32,6 +34,8 @@ function App() {
             {/* privates routes */}
             <Route path="/admin" element={<RutaProtegida />}>
               <Route index element={<AdministrarPacientes />} />
+              <Route path="perfil" element={<EditarPerfil />} />
+              <Route path="cambiar-password" element={<CambiarPassword />} />
             </Route>
           </Routes>
         </PacientesProvider>
---
+++ frontend/src/components/AdminNav.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const AdminNav = () => {
+  return (
+    <div className="flex gap-3">
+      <Link to="/admin/perfil" className="font-bold uppercase text-gray-500">
+        Perfil
+      </Link>
+      <Link
+        to="/admin/cambiar-password"
+        className="font-bold uppercase text-gray-500"
+      >
+        Cambiar Password
+      </Link>
+    </div>
+  );
+};
+
+export default AdminNav;
